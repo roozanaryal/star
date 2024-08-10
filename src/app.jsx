5517375e2a51d0cab56316241290a7e8284c7736
Rojan Aryal <roozanaryal8@gmail.com>
2024-08-10 11:00:36 +0545

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,40 @@
+import { useState } from "preact/hooks";
+import "./app.css";
+import { FaRegStar } from "react-icons/fa";
+
+
+export function App({ noOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+
+        return (
+          <FaRegStar
+            key={index}
+            className={index <=(hover||rating)?'active':'inactive'}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+}
